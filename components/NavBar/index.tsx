0399baa8f43b8155ccc2c63b604537de56fe779c
Ailton Duarte <adidas.coyas@gmail.com>
2021-11-16 15:02:39 -0100

    add logo to body

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -106,7 +106,7 @@ const NavBar = () => {
 					<div className="flex justify-start lg:w-0 lg:flex-1">
 						<a href="#">
 							<span className="sr-only">Workflow</span>
-							<img className="h-8 w-auto sm:h-10" src="/logo40.png" alt="" />
+							<img className="h-8 w-auto sm:h-10" src="/logo.png" alt="" />
 						</a>
 					</div>
 					<div className="-mr-2 -my-2 md:hidden">
@@ -329,11 +329,7 @@ const NavBar = () => {
 						<div className="pt-5 pb-6 px-5">
 							<div className="flex items-center justify-between">
 								<div>
-									<img
-										className="h-8 w-auto"
-										src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-										alt="Workflow"
-									/>
+									<img className="h-8 w-auto" src="/logo.png" alt="Workflow" />
 								</div>
 								<div className="-mr-2">
 									<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
