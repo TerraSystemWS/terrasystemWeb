f6cb312bee71045455cedb7af651a2e23e22278f
AIlton Duarte <adidas.coyas@gmail.com>
2023-09-28 15:57:14 -0100

    create footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,20 +1,20 @@
 import styles from "../../styles/Home.module.css";
-import Image from "next/image";
+
 
 const Footer = () => {
 	return (
-		<footer className={styles.footer}>
-			<a
-				href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				Powered by TerraSystem &&{" "}
-				<span className={styles.logo}>
-					<Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-				</span>
-			</a>
-		</footer>
+		<footer className="text-center  p-4 fixed bottom-0 w-full">
+		<div className="flex items-center justify-center">
+		  <a
+			href="https://terrasystem.cv"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="text-gray-800 font-bold"
+		  >
+			Powered by TerraSystem{" "}
+		  </a>
+		</div>
+	  </footer>
 	);
 };
 
