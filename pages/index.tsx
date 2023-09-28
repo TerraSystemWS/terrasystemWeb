767b1a25316de7f94d1ecdd1ce5e3300b730f7ba
AIlton Duarte <adidas.coyas@gmail.com>
2023-09-28 15:58:34 -0100

    feat: home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,121 +1,21 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import Head from "next/head";
-import Image from 'next/image'
+import Layout from '../components/Layout';
 
 const navigation = [
 	{ name: 'Serviços', href: '/servicos' },
 	{ name: 'Docs & Posts', href: '/repository' },
 	{ name: 'Marketplace', href: '/marketplace' },
-	{ name: 'Sobre', href: '/sobre' },
+	{ name: 'Sobre', href: '/about' },
 ]
 
-export default function Example() {
-	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
+  
 
+const Home: React.FC = () => {
 	return (
-		<div>
-			<Head>
-				<title>TerraSystem, Soluções Tech e Web</title>
-			</Head>
-			<div className="bg-white overflow-hidden">
-				<header className="absolute inset-x-0 top-0 z-50">
-					<nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-						<div className="flex lg:flex-1">
-							<Link href="/" >
-								<a className="-m-1.5 p-1.5 flex items-center space-x-4">
-									<Image
-										className="h-14 w-auto"
-										src="https://res.cloudinary.com/coyas/image/upload/v1691545378/terrasystem/terrasystemweb/logo-sembg-black_cnthas.png"
-										alt="Logo"
-										width={50}
-										height={50}
-									/>
-									<span className="ml-auto text-md font-semibold text-black" >TerraSystem</span>
-								</a>
-							</Link>
-						</div>
-						<div className="flex lg:hidden">
-							<button
-								type="button"
-								className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-								onClick={() => setMobileMenuOpen(true)}
-							>
-								<span className="sr-only">Open main menu</span>
-								<Bars3Icon className="h-6 w-6" aria-hidden="true" />
-							</button>
-						</div>
-						<div className="hidden lg:flex lg:gap-x-12">
-							{navigation.map((item) => (
-
-								<Link key={item.name} href={item.href}><a className="text-sm font-semibold leading-6 text-gray-900">
-									{item.name}
-								</a></Link>
-							))}
-						</div>
-						<div className="hidden lg:flex lg:flex-1 lg:justify-end">
-							<Link href="/login">
-								<a className="text-sm font-semibold leading-6 text-gray-900">
-									Log in <span aria-hidden="true">&rarr;</span>
-								</a></Link>
-						</div>
-					</nav>
-					<Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-						<div className="fixed inset-0 z-50" />
-						<div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-							<div className="flex items-center justify-between">
-								<Link href="/">
-									<a className="-m-1.5 p-1.5">
-										<Image
-											className="h-8 w-auto"
-											src="https://res.cloudinary.com/coyas/image/upload/v1691545378/terrasystem/terrasystemweb/logo-sembg-black_cnthas.png"
-											alt="logo"
-											width={40}
-											height={40}
-										/>
-
-									</a></Link>
-								<button
-									type="button"
-									className="-m-2.5 rounded-md p-2.5 text-gray-700"
-									onClick={() => setMobileMenuOpen(false)}
-								>
-									<span className="sr-only">Close menu</span>
-									<XMarkIcon className="h-6 w-6" aria-hidden="true" />
-								</button>
-							</div>
-							<div className="mt-6 flow-root">
-								<div className="-my-6 divide-y divide-gray-500/10">
-									<div className="space-y-2 py-6">
-										{navigation.map((item) => (
-											<Link key={item.name}
-												href={item.href}>
-												<a
-													className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7  hover:bg-gray-50"
-												>
-													{item.name}
-												</a></Link>
-										))}
-									</div>
-									<div className="py-6">
-										<Link href="/login">
-											<a
-												className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7  hover:bg-gray-50"
-											>
-												Log in
-											</a>
-										</Link>
-									</div>
-								</div>
-							</div>
-						</div>
-					</Dialog>
-				</header>
-
-				<div className="relative isolate px-6 pt-14 lg:px-8">
+		<Layout navigation={navigation} title='TerraSystem, Soluções Tech e Web'>
+				{/* aqui começa o corpo */}
+				
 					<div
 						className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
 						aria-hidden="true"
@@ -172,8 +72,11 @@ export default function Example() {
 							}}
 						/>
 					</div>
-				</div>
-			</div>
-		</div>
+				
+				{/* fim do corpo */}
+			
+		</Layout>
 	)
 }
+
+export default Home
